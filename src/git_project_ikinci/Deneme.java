c54959a1ee2f368d20beb22bc5c936865431df02
mehmetkoksal61 <69307065+mehmetkoksal61@users.noreply.github.com>
2020-08-09 22:29:20 +0200

    Update Deneme.java
    Mehmetten selamlar

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -7,6 +7,7 @@ public class Deneme {
 		System.out.println("Ikinci satir kodum");
 		System.out.println("remote degisiklik");
 		System.out.println("Ben Mehmet");
+		System.out.println("Cakisma - Mehmet");
 
 	}
 
