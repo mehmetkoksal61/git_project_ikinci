aba6770f0d9146c7ab906c1e2385f16870bd52c3
unknown <mehmetkoksal61@gmail.com>
2020-08-09 22:11:59 +0200

    ikinci satir ekledim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -3,7 +3,8 @@ package git_project_ikinci;
 public class Deneme {
 
 	public static void main(String[] args) {
-		System.out.println("GIT YENI PROJEYE");
+		System.out.println("Ikinci git projem");
+		System.out.println("Ikinci satir kodum");
 
 	}
 
